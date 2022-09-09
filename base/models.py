276813cdae00bbf36b774508f9b406c997f611bd
Gvdien <giangvinhdien.tdtu@gmail.com>
2022-09-09 21:56:44 +0700

    Database

--- base/models.py
+++ base/models.py
@@ -1,6 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 # Create your models here.
-class room(models.Model):
-    name = models.CharField(max_length= 100)
+class Room(models.Model):
+    host = models.ForeignKey(User, on_delete=models.SET_NULL,null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=100)
     description = models.TextField(max_length=100)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    message = models.TextField(max_length=100)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.message
--- base/views.py
+++ base/views.py
@@ -1,23 +1,21 @@
 from django.shortcuts import render
-
+from .models import Room
 # Create your views here.
 
 
 rooms = [
-    {"id":1,"Name":"Page 1"},
-    {"id":2,"Name":"Page 2"},
-    {"id":3,"Name":"Page 3"}
+    {"id":1,"name":"Page 1"},
+    {"id":2,"name":"Page 2"},
+    {"id":3,"name":"Page 3"}
 ]
 
 
 def home(req):
+    rooms = Room.objects.all()
     context = {"rooms": rooms}
     return render(req, "home.html", context)
 
 def room(req,key):
-    room = None
-    for i in rooms:
-        if i['id'] == int(key):
-            room = i
+    room = Room.objects.get(id=key)
     context = {'room':room}
     return render(req, 'room.html',context)
